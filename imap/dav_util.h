793c5ab02aeeb144c6e6467129ab5d9f08e64d69
Ken Murchison <murch@fastmail.com>
2021-12-14 15:40:31 -0500

    DAV: move carddav_remove() from carddav_db.c to dav_util.c as dav_remove_resource()

--- imap/dav_util.h
+++ imap/dav_util.h
@@ -147,4 +147,7 @@ int dav_store_resource(struct transaction_t *txn,
                        struct mailbox *mailbox, struct index_record *oldrecord,
                        modseq_t createdmodseq, strarray_t *imapflags);
 
+int dav_remove_resource(struct mailbox *mailbox,
+                        uint32_t olduid, int isreplace, const char *userid);
+
 #endif /* DAV_UTIL_H */
